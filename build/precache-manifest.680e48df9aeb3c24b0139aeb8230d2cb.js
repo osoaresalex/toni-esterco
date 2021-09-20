44d81c7d58e9e8c69ef6fda0fd453a25778049cd
Alex <multiwebagencia@gmail.com>
2021-09-20 10:35:58 -0300

    s

---
+++ build/precache-manifest.680e48df9aeb3c24b0139aeb8230d2cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f8ccb2e843452b304e1",
+    "url": "/code-hero/static/css/main.92b94933.chunk.css"
+  },
+  {
+    "revision": "5f8ccb2e843452b304e1",
+    "url": "/code-hero/static/js/main.11af14f7.chunk.js"
+  },
+  {
+    "revision": "4ff737e2ed0f7fc65274",
+    "url": "/code-hero/static/js/runtime~main.2bdaedf3.js"
+  },
+  {
+    "revision": "e4b5556a5a75a76889cb",
+    "url": "/code-hero/static/js/2.5e3e3dde.chunk.js"
+  },
+  {
+    "revision": "c73de0f075fb4995dc463338b6caf71f",
+    "url": "/code-hero/index.html"
+  }
+];
